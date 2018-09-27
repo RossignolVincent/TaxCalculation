f4b81f7f880e507069735a677c70ea3fc41853f8
formation <rossignol.vince@gmail.com>
2018-09-27 10:36:42 +0200

    Negative revenue excepetion

--- src/main/java/com/soat/tax/services/TaxCalculationImpl.java
+++ src/main/java/com/soat/tax/services/TaxCalculationImpl.java
@@ -1,11 +1,15 @@
 package com.soat.tax.services;
 
 import com.soat.tax.company.Company;
+import com.soat.tax.exceptions.IllegalSalesRevenueException;
 
 public class TaxCalculationImpl implements TaxCalculation {
 
     @Override
     public float calculateTax(Company company, float salesRevenue) {
+        if(salesRevenue < 0){
+            throw new IllegalSalesRevenueException();
+        }
         return company.getTaxPercentage() * salesRevenue;
     }
 }
--- src/test/java/com/soat/tax/services/TaxCalculationImplTest.java
+++ src/test/java/com/soat/tax/services/TaxCalculationImplTest.java
@@ -3,6 +3,7 @@ package com.soat.tax.services;
 import com.soat.tax.company.AutoCompany;
 import com.soat.tax.company.Company;
 import com.soat.tax.company.SASCompany;
+import com.soat.tax.exceptions.IllegalSalesRevenueException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -20,5 +21,12 @@ public class TaxCalculationImplTest {
         Company sasCompany = new SASCompany("012345", "Soat", "Rue des frigos");
         TaxCalculation t = new TaxCalculationImpl();
         Assert.assertEquals(330f, t.calculateTax(sasCompany, 1000f), 0);
+    }
+
+    @Test(expected = IllegalSalesRevenueException.class)
+    public void should_throw_illegal_sales_revenue_exception() {
+        Company autoCompany = new AutoCompany("012345", "Soat");
+        TaxCalculation t = new TaxCalculationImpl();
+        t.calculateTax(autoCompany, -1000f);
     }
 }
